d26fa7f1bdf687af0ed923682c24f3c2b2c2ed54
andrei.pugatsov <andrei.pugatsov@gmail.com>
2024-10-08 12:21:41 +0300

    Retrieve file list by uploader - DTO

---
+++ src/main/java/com/example/file_upload/dto/FileDto.java
@@ -0,0 +1,27 @@
+package com.example.file_upload.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class FileDto {
+    private Long id;
+    private String fileName;
+    private String fileType;
+    private String uploader;
+    private LocalDateTime uploadedAt;
+    private String metadata;
+
+    // Constructor
+    public FileDto(Long id, String fileName, String fileType, String uploader, LocalDateTime uploadedAt, String metadata) {
+        this.id = id;
+        this.fileName = fileName;
+        this.fileType = fileType;
+        this.uploader = uploader;
+        this.uploadedAt = uploadedAt;
+        this.metadata = metadata;
+    }
+}
